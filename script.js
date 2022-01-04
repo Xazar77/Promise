87e9a43d0bafe537330a1d7f707638551c87239c
Anderey Prokhorov <beta737@yandex.ru>
2022-01-04 16:03:35 +0300

    26 урок

---
+++ script.js
@@ -0,0 +1,48 @@
+//  'use strict';
+ 
+//  let user ={};
+ 
+ const getData = () => {
+     return fetch('db.json')
+        .then(res => res.json())
+        .then(data => {
+           render(data);
+        })
+        .catch(error => console.log(error));
+ };
+ getData();
+ const render = (data) => {
+     
+    localStorage.setItem('user', JSON.stringify(data));
+ };
+ 
+
+ if(localStorage.getItem('user')) {
+     const user = JSON.parse(localStorage.getItem('user'));
+     const sendData = (url, data) => {
+     return fetch(url, {
+         method: 'POST',
+         body: data,
+         headers: {
+             'Content-type': 'application/json; charset=UTF-8'
+         },
+
+     })
+     .then(res => res.json())
+     .then(data => console.log(data))
+     .catch(error => console.log(error));
+    };
+
+ sendData('https://jsonplaceholder.typicode.com/posts', JSON.stringify(user));
+ }
+ 
+ 
+    
+
+       
+
+
+    
+
+    
+ 
